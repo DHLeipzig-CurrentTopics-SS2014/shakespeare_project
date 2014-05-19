c0becae1bf139385a6053c94bf4861ca204c39a9
telzul <robert.terbach@googlemail.com>
2014-05-19 23:56:56 +0200

    continued work

--- parser/lampeter_parser.py
+++ parser/lampeter_parser.py
@@ -16,6 +16,17 @@ def find_text(content):
 	pattern = re.compile('(<body>)(.*)(</body>)', re.IGNORECASE | re.DOTALL)
 	return pattern.search(content).groups()[1]
 
+def clean_from_nonletters(text):
+    return re.sub(r'[^a-zA-Z\s]', ' ', text)
+
+def clean_from_markup(text):
+	pattern = re.compile('<.*?>', re.IGNORECASE | re.DOTALL)
+    return pattern.sub('', text)
+    
+def clean_from_entities(text):
+    pattern = re.compile('&[a-zA-Z]{1,5};', re.IGNORECASE)
+    return pattern.sub('', text)
+
 # Text ist im <TEXT> tag enthalten
 # bereinigen von tags und entities (&rehy;, <RO>, <P>, <NOTE>, jede menge anderes)
 # danach alle satzzeichen und zahlen entfernen
