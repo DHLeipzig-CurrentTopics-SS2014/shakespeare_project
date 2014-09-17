7df6e66803fcb4d70c70c975f93d3f5062af7e8d
marlux <mai13dzu@studserv.uni-leipzig.de>
2014-09-17 22:08:24 +0200

    Meisten Wörter in einen Buch
    Hinweis da Barchart nih funktioniert liefert das ganze noch ein Ergebnis wird am Donnerstag besprochen

--- src/django_test/calculations/questions/booksMostShakeWords.py
+++ src/django_test/calculations/questions/booksMostShakeWords.py
@@ -1,3 +1,6 @@
+from calculations import util
+from corpora.models import *
+import re
 
 class BookWithMostWords:
     def url(self):
@@ -12,5 +15,24 @@ class BookWithMostWords:
         return "What could this mean?"
     
     def calc(self, parsed_request):
-        # your calculations
-        return {'x': '[]', 'y':'[]'}
+        
+        texts = util.get_texts_from_authors_in_timespan(parsed_request['authors'], parsed_request['timespan'])
+        words = parsed_request['words']
+    
+        wctxts={}
+
+        for text in texts:
+            wtcs = WordInTextCount.objects.filter(text=text)
+            count=0
+            for word in words:
+                counts = wtcs.filter(word__word=word)
+                if len(counts) > 0:
+                    count += counts.first().count
+            wctxts[text.title]=count
+        
+        wctxts=sorted(wctxts.items(), key=lambda x: x[1])
+        
+        xs=[author for author, ratio in wctxts]
+        ys =[ratio for author, ratio in wctxts]
+
+        return {'x': xs, 'y': ys}
