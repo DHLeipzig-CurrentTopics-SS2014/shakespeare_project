24134b8165ecc9fd54d4aca696cec4baeab4c913
Lukas Fischer <joki0z@web.de>
2014-09-30 17:59:28 +0200

    parse_all will now parse all five corpora

--- src/parser/parse_all.py
+++ src/parser/parse_all.py
@@ -3,6 +3,15 @@
 #
 # usage: ./parse_all.py <corpus_dir>
 #
+# <corpus_dir> must contain the following files:
+# 2468.zip
+# 2507.zip
+# cen.zip
+# clmet3_0.zip
+# Lampeter_txt.zip
+#
+# the parsed files are stored in "<corpus_dir>/xml"
+#
 ####################################
 
 import lampeter_parser
@@ -14,19 +23,12 @@ import os, sys
 
 def main(corpus_dir):
     result_dir = os.path.join(corpus_dir,"xml")
-    for e in os.listdir(corpus_dir):
-        if e[-4:] != ".zip":
-            continue
-        if "cen" in e:
-            cen_parser.main(e, result_dir)
-        if "clmet" in e:
-            clmet.main(e, result_dir)
-        if "ampeter" in e:
-            lampeter_parser.main(e, result_dir)
-        if "2507" in e:
-            parse_2507.main(e, result_dir)
-        if "2468" in e:
-            parse_2468.main(e, result_dir)
+    cen_parser.main(os.path.join(corpus_dir, "cen.zip"), os.path.join(result_dir, "cen"))
+    clmet.main(os.path.join(corpus_dir, "clmet3_0.zip"), os.path.join(result_dir, "clmet"))
+    lampeter_parser.main(os.path.join(corpus_dir, "Lampeter_txt.zip"), os.path.join(result_dir, "lampeter"))
+    parse_2507.main(os.path.join(corpus_dir, "2507.zip"), os.path.join(result_dir, "2507"))
+    parse_2468.main(os.path.join(corpus_dir, "2468.zip"), os.path.join(result_dir, "2468"))
+
 if __name__ == "__main__":
     main(sys.argv[1])
 
